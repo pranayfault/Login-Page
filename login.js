2e286848a3b08d6f65c423468218ba4b033b3dc3
PRANAY PATHAK <128687852+pranayfault@users.noreply.github.com>
2024-03-03 17:43:46 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,1 @@
-function validate()
-               {
-               var email = document.getElementById("email"). value;
-               var password = document.getElementById("password"). value;
-               if(email== "pathakprasumitra@gmail.com" && password=="Pranay123")
-               {
-                   
-                   alert ("login successfully");
-                   return false;
 
-              
-               }
-               else{
-                   alert ("🤡");
-                   return;
-               } 
