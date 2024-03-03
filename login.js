cd2d27db8c6a3739b8d3c33b0ecf82c9d789b8b3
PRANAY PATHAK <128687852+pranayfault@users.noreply.github.com>
2024-03-03 16:50:34 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+function validate()
+               {
+               var email = document.getElementById("email"). value;
+               var password = document.getElementById("password"). value;
+               if(email== "pathakprasumitra@gmail.com" && password=="Pranay123")
+               {
+                   
+                   alert ("login successfully ");
+               }
+               else{
+                   alert ("badk land lala mera🤡 ");
+                   return;
+               } 
